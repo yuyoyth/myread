7fb27f8eee73d78a17d20cb59ffd109bc86ea7ca
Yao Tinghao <yuyoyth@gmail.com>
2018-02-07 02:24:11 +0800

    新功能：书籍从服务器上获取
    修改
    Book--提取共同特征为属性，作为书籍渲染控件使用
    BookShelf--提取共同特征作为属性，作为书架渲染控件使用
    BooksShowcase--从服务器获取加入列表的数据，对三个书架进行渲染
    
    删除
    BookState--不太需要

--- src/Book.js
+++ src/Book.js
@@ -7,19 +7,37 @@ import PropTypes from 'prop-types'
 
 class Book extends Component {
   static propTypes = {
-    data: PropTypes.instanceOf(Promise)
+    id: PropTypes.string.isRequired,
+    imageUrl: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    authors: PropTypes.array.isRequired,
+    shelf: PropTypes.string.isRequired
+  }
+
+  state = {
+    shelf: 'None'
   }
 
   render() {
+    const {id, imageUrl, title, authors, shelf} = this.props
+
     return(
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-
+            <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: `url("${imageUrl}")` }}></div>
+            <div className="book-shelf-changer">
+              <select defaultValue={shelf}>
+                <option value="none" disabled="disabled">Move to...</option>
+                <option value="currentlyReading" disabled={'currentlyReading' === shelf ? 'disabled' : ''}>Currently Reading</option>
+                <option value="wantToRead" disabled={'wantToRead' === shelf ? 'disabled' : ''}>Want to Read</option>
+                <option value="read" disabled={'read' === shelf ? 'disabled' : ''}>Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
           </div>
-          <div className="book-title">Ender's Game</div>
-          <div className="book-authors">Orson Scott Card</div>
+          <div className="book-title">{title}</div>
+          <div className="book-authors">{authors.join()}</div>
         </div>
       </li>
     )
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -4,54 +4,44 @@
  */
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import Book from './Book'
 
 class BookShelf extends Component {
   static propTypes = {
+    kind: PropTypes.string.isRequired,
+    bookList: PropTypes.array.isRequired
+  }
 
+  getTitle(kind) {
+    switch (kind) {
+      case 'currentlyReading':
+        return 'Currently Reading';
+      case 'wantToRead':
+        return 'Want to Read';
+      case 'read':
+        return 'Read';
+      default:
+        return 'None';
+    }
   }
 
   render() {
+    const {kind, bookList} = this.props
+
     return(
       <div className="bookshelf">
-        <h2 className="bookshelf-title">Currently Reading</h2>
+        <h2 className="bookshelf-title">{this.getTitle(kind)}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">To Kill a Mockingbird</div>
-                <div className="book-authors">Harper Lee</div>
-              </div>
-            </li>
-            <li>
-              <div className="book">
-                <div className="book-top">
-                  <div className="book-cover" style={{ width: 128, height: 188, backgroundImage: 'url("http://books.google.com/books/content?id=yDtCuFHXbAYC&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE72RRiTR6U5OUg3IY_LpHTL2NztVWAuZYNFE8dUuC0VlYabeyegLzpAnDPeWxE6RHi0C2ehrR9Gv20LH2dtjpbcUcs8YnH5VCCAH0Y2ICaKOTvrZTCObQbsfp4UbDqQyGISCZfGN&source=gbs_api")' }}></div>
-                  <div className="book-shelf-changer">
-                    <select>
-                      <option value="none" disabled>Move to...</option>
-                      <option value="currentlyReading">Currently Reading</option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
-                  </div>
-                </div>
-                <div className="book-title">Ender's Game</div>
-                <div className="book-authors">Orson Scott Card</div>
-              </div>
-            </li>
+            {bookList.map(book =>
+              <Book
+                key={book.id}
+                id={book.id}
+                imageUrl={book.imageUrl}
+                title={book.title}
+                authors={book.authors}
+                shelf={book.shelf}/>
+            )}
           </ol>
         </div>
       </div>
